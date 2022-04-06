0638ca4b8f44bd3829dfd9956325a56ffaa7bd94
Alejandro <ajc9896@nyu.edu>
2022-04-06 19:05:17 -0400

    updates

--- solution.py
+++ solution.py
@@ -52,7 +52,7 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         # Fetch the ICMP header from the IP packet
 
         icmpHeader = recPacket[20:28]
-        icmpType, code, mychecksum, packedID, sequence = struct.unpack("bbHHh", icmpHeader)
+        icmpType, code, myChecksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
 
         if type != 8 and packetID == ID:
             bytesinDouble = struct.calcsize("d")
